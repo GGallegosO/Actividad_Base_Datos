b541046891dcf07106550c0de7b79744fbb88ddf
Gustavo <gustavogallegosordenes@gmail.com>
2025-08-16 01:45:25 -0400

    Primeros comandos SQL

---
+++ initial_commands.sql
@@ -0,0 +1,14 @@
+-- Comentario de una sola línea
+
+/*
+Este es un comentario
+que se extiende por
+varias líneas.
+*/
+
+
+SHOW DATABASES;  -- Para ver las bases de datos disponibles.
+
+USE world; -- Para seleccionar la base de datos world.
+
+SHOW TABLES; -- Para ver las tablas dentro de la base de datos world.
